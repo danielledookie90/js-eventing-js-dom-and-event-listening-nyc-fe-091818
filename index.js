9f3ca1c2abec78c1768a32efa2c1e24bc5d76111
Danielle Dookie <danielledookie90@gmail.com>
2018-10-30 22:36:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+//1. get the element 
+ const main = document.querySelector('main')
+
+//2. add an event listener
+function onClick(event){
+  alert('i was clicked woah')
+}
+
+main.addEventListener('click', onClick)
+
+function onKeyDown(event){
+  if (event.which === 74){
+   event.preventDefault()
+  }
+}
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', onKeyDown )
