2b48bc11c682ec6d855c3d33af510003035a8a43
Danielle Dookie <danielledookie90@gmail.com>
2018-10-30 22:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ input.addEventListener('keydown', onKeyDown )
 const divs = document.querySelectorAll('div')
 console.log(divs)
 
+divs[2] .addEventListener('click', onDivClick)
+
 function onDivClick(event){
   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
 }
